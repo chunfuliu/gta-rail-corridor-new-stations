314c85b413992398348533a7abc95c97d6c65f5a
dlm589 <chunfu.liu12@gmail.com>
2025-10-08 01:58:58 -0400

    Updated svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
+
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/gta-rail-corridor-new-stations",
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+    }
 };
 
 export default config;
